b70edd67fce4f11679c998b884c010de3a6eac9b
AndrewParkhomov <parkhomov.andrew@gmail.com>
2018-10-17 10:35:45 +0300

    - Version 3.0.1 - Fix bug with vector drawable for android 4.4.2 and 4.4.4

--- app/src/main/java/parkhomov/andrew/lensthicknesscalculator/MyApp.kt
+++ app/src/main/java/parkhomov/andrew/lensthicknesscalculator/MyApp.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
+import android.support.v7.app.AppCompatDelegate
+import org.koin.standalone.StandAloneContext.startKoin
 import parkhomov.andrew.lensthicknesscalculator.di.lensThicknessCalculatorApp
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
@@ -29,5 +31,13 @@ class MyApp : Application() {
 
         // start Koin context
         startKoin(this, lensThicknessCalculatorApp)
+    }
+
+    companion object {
+        // for pre lollipop
+        // allow to use drawableLeft/Right
+        init {
+            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        }
     }
 }
